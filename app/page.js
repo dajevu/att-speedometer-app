f71fa541b051d7729196fa96c8e022e6437213d6
Jeff Davis <jeff.davis@marketcast.com>
2024-11-14 20:05:03 -0700

    Updated to use app router

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import Speedometer from './components/Speedometer';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Speedometer Example</h1>
+      <Speedometer />
+    </div>
+  );
+}
